1f1e2abaf5aafea2a8b06a10634a9a0b581a63d3
Sami Bouzaien <sami.bouzaien10@gmail.com>
2021-10-13 22:07:20 -0400

    Ajouter scripts powershell

---
+++ script-powershell/get_list_apps_ps.ps1
@@ -0,0 +1,12 @@
+﻿
+#Software Installed
+
+$computers=Get-Content C:\Temp\computers.txt
+
+ForEach($computer in $computers){
+
+Write-Host "Processing Server $computer"
+
+Get-WmiObject -Class Win32_Product -ComputerName $computer | select __SERVER , Name, Version -ErrorAction SilentlyContinue
+
+}
