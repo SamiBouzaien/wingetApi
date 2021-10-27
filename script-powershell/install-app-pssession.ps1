1d4a7d90e933daa8dea37efb89d02c5184462a0e
Sami Bouzaien <sami.bouzaien10@gmail.com>
2021-10-27 13:01:23 -0400

    ajouter swagger

--- script-powershell/install-app-pssession.ps1
+++
@@ -1,8 +0,0 @@
-﻿
-$session = New-PSSession -ComputerName 192.168.2.20
-Copy-Item -Path c:\Install_source\notepad_install.exe -ToSession $session -Destination 'c:\Temp\installer.exe'
-
-Invoke-Command -Session $session -ScriptBlock {
-    Start-Process c:\Temp\installer.exe -Wait
-}
-Remove-PSSession $session
